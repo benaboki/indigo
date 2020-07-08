3e1d3d0238a413a25e07c2c56770f7be71f745e4
Benedict Omare <benaboki@gmail.com>
2020-07-08 17:19:02 +0200

    production.py

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # Configure default domain name
-ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.akn4undocs.ipbes.net', '127.0.0.1'] if 'WEBSITE_SITE_NAME' in os.environ else []
+#ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.akn4undocs.ipbes.net', '127.0.0.1'] if 'WEBSITE_SITE_NAME' in os.environ else []
 #ALLOWED_HOSTS = ['indigo.akn4undocs.ipbes.net', 'www.indigo.akn4undocs.ipbes.net','127.0.0.1', 'localhost', '172.17.0.5']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
