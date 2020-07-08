a68dd8f3739b33d26ff78bcd594b376f04011ec7
Benedict Omare <benaboki@gmail.com>
2020-07-08 16:58:54 +0200

    remove whitenoise from production

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -4,12 +4,6 @@ from .base import *
 #ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.akn4undocs.ipbes.net', '127.0.0.1'] if 'WEBSITE_SITE_NAME' in os.environ else []
 ALLOWED_HOSTS = ['indigo.akn4undocs.ipbes.net', 'www.indigo.akn4undocs.ipbes.net','127.0.0.1']
 
-# WhiteNoise configuration
-MIDDLEWARE = [                                                                                              
-    'whitenoise.middleware.WhiteNoiseMiddleware',                                     
-]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configure Postgres database
