f552c83ebadc4e2385fc44dcf08f298cc62cea55
Benedict Omare <benaboki@gmail.com>
2020-07-10 21:11:22 +0200

    production.py > re-enable STATIC_ROOT

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # Configure default domain name
 ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.akn4undocs.ipbes.net', '127.0.0.1', '172.17.0.5', 'localhost'] if 'WEBSITE_SITE_NAME' in os.environ else []
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configure Postgres database
 db_from_env = dj_database_url.config(conn_max_age=500)
